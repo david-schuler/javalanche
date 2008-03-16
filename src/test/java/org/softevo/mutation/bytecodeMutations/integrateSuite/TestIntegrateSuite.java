dbeaf30dc1c53b115e2b92f37b7a85fe06165a98
schuler <devnull@localhost>
2008-03-16 19:30:32 +0000

    changed phase for javaagent creation

---
+++ src/test/java/org/softevo/mutation/bytecodeMutations/integrateSuite/TestIntegrateSuite.java
@@ -0,0 +1,26 @@
+package org.softevo.mutation.bytecodeMutations.integrateSuite;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import junit.framework.TestCase;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+
+import org.junit.Test;
+import org.softevo.mutation.bytecodeMutations.integrateSuite.testClasses.AllTests;
+import org.softevo.mutation.runtime.SelectiveTestSuite;
+
+public class TestIntegrateSuite {
+
+	@Test
+	public void runTests() {
+		TestSuite suite = AllTests.suite();
+		Map<String, TestCase> map = SelectiveTestSuite.getAllTests(suite);
+		suite.run(new TestResult());
+		assertTrue(suite instanceof SelectiveTestSuite);
+		assertTrue(map.size() > suite.testCount());
+	}
+
+}
