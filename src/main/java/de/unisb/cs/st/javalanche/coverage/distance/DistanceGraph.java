a5a89e4211e9d818139e67ed2f5ef00eb397f525
schuler <devnull@localhost>
2009-10-06 08:41:05 +0000

    added missing class

---
+++ src/main/java/de/unisb/cs/st/javalanche/coverage/distance/DistanceGraph.java
@@ -0,0 +1,89 @@
+package de.unisb.cs.st.javalanche.coverage.distance;
+
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+public class DistanceGraph {
+
+	private static Logger logger = Logger.getLogger(DistanceGraph.class);
+	// private ConnectionData data;
+	private final Hierarchy hierarchy;
+	private UndirectedGraph<MethodDescription, DefaultEdge> g;
+	private Set<MethodDescription> allMethods;
+
+	public DistanceGraph(ConnectionData data, Hierarchy hierarchy) {
+		// this.data = data;
+		this.hierarchy = hierarchy;
+		allMethods = data.getAllMethods();
+		g = new SimpleGraph<MethodDescription, DefaultEdge>(DefaultEdge.class);
+		Set<Tuple> connections = data.getConnections();
+		for (Tuple tuple : connections) {
+			MethodDescription start = tuple.getStart();
+			MethodDescription end = tuple.getEnd();
+			if (!start.equals(end)) {
+				if (!g.containsVertex(start)) {
+					g.addVertex(start);
+				}
+				if (!g.containsVertex(end)) {
+					g.addVertex(end);
+				}
+				g.addEdge(start, end);
+			}
+		}
+	}
+
+	public int getDistance(MethodDescription start, MethodDescription end) {
+		if (!(g.containsVertex(start) && g.containsVertex(end))) {
+			return -1;
+		}
+		DijkstraShortestPath<MethodDescription, DefaultEdge> sp = new DijkstraShortestPath<MethodDescription, DefaultEdge>(
+				g, start, end);
+		double pathLength = sp.getPathLength();
+		if (pathLength == Double.POSITIVE_INFINITY) {
+			pathLength = 0;
+			Set<String> allSupers = hierarchy.getAllSupers(end.getClassName());
+			for (String sup : allSupers) {
+				MethodDescription super1 = end.getSuper(sup);
+				int distance = getDistance(start, super1);
+				if (distance > 0) {
+					return distance;
+				}
+			}
+		}
+		if (pathLength < 0.) {
+			pathLength = 0;
+		}
+		return (int) pathLength;
+	}
+
+	public static DistanceGraph getDefault() {
+		ConnectionData data = ConnectionData.read();
+		Hierarchy hierarchy = Hierarchy.readFromDefaultLocation();
+		return new DistanceGraph(data, hierarchy);
+	}
+
+	public MethodDescription getMetodDesc(String fullMethodName) {
+		String className = fullMethodName.substring(0, fullMethodName
+				.indexOf('@'));
+		String methodName = fullMethodName.substring(fullMethodName
+				.indexOf('@') + 1);
+
+		for (MethodDescription md : allMethods) {
+			if (className.equals(md.getClassName())) {
+				if (methodName.equals(md.getMethodName())) {
+					return md;
+				}
+			}
+
+		}
+		logger.warn("Not Found " + className + "   " + methodName + " "
+				+ fullMethodName);
+		return null;
+	}
+
+}
---
+++ src/main/java/de/unisb/cs/st/javalanche/mutation/analyze/RandomUndetectedAnalyzer.java
@@ -0,0 +1,96 @@
+/*
+* Copyright (C) 2009 Saarland University
+* 
+* This file is part of Javalanche.
+* 
+* Javalanche is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Javalanche is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser Public License
+* along with Javalanche.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package de.unisb.cs.st.javalanche.mutation.analyze;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import de.unisb.cs.st.ds.util.io.Io;
+import de.unisb.cs.st.javalanche.mutation.analyze.html.HtmlReport;
+import de.unisb.cs.st.javalanche.mutation.results.Mutation;
+
+// -Djavalanche.mutation.analyzers=de.unisb.cs.st.javalanche.mutation.analyze.RandomUndetectedAnalyzer 
+public class RandomUndetectedAnalyzer implements MutationAnalyzer {
+
+	private static Logger logger = Logger
+			.getLogger(RandomUndetectedAnalyzer.class);
+
+	public String analyze(Iterable<Mutation> mutations, HtmlReport report) {
+		List<Mutation> undetected = new ArrayList<Mutation>();
+		Set<String> allClasses = new HashSet<String>();
+		for (Mutation mutation : mutations) {
+			if (mutation.getMutationResult() != null) {
+				if (!mutation.isKilled()) {
+					undetected.add(mutation);
+					String clazz = getContainingClass(mutation.getClassName());
+					allClasses.add(clazz);
+				}
+			}
+		}
+		Random r = new Random();
+		StringBuilder sb = new StringBuilder();
+		List<String> classes = new ArrayList<String>();
+		int i = 1;
+		StringBuilder sbcsv = new StringBuilder();
+		List<Mutation> second = new ArrayList<Mutation>();
+		while (i <= 50 && undetected.size() > 0) {
+			int index = r.nextInt(undetected.size());
+			Mutation mutation = undetected.remove(index);
+
+			String clazz = getContainingClass(mutation.getClassName());
+			if (!classes.contains(clazz)) {
+				classes.add(clazz);
+				sb.append(String.format("%2d. ", i));
+				sb.append(mutation);
+				sbcsv.append(mutation.getCsvString());
+				sbcsv.append('\n');
+				for (int j = 0; j < 80; j++) {
+					sb.append('-');
+				}
+				i++;
+			} else {
+				second.add(mutation);
+			}
+			if (classes.size() == allClasses.size() || undetected.size() == 0) {
+				logger.info("Got mutation from every class - next round");
+				classes.clear();
+				undetected.addAll(second);
+				second.clear();
+			}
+		}
+		Io.writeFile(sbcsv.toString(), new File("random-undetected.csv"));
+		Io.writeFile(sb.toString(), new File("random-undetected.txt"));
+		return sb.toString();
+	}
+
+	private String getContainingClass(String className) {
+		if (className.contains("$")) {
+			int i = className.indexOf('$');
+			return className.substring(0, i);
+		}
+		return className;
+	}
+
+}
