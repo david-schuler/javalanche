b0b0765ad5f65a05b23ebaee04bde528c57519e0
schuler <devnull@localhost>
2010-07-28 10:56:54 +0000

    added data again

---
+++ src/test/java/de/unisb/cs/st/javalanche/mutation/runtime/testDriver/junit/data/TestCaseForJunit4Test.java
@@ -0,0 +1,23 @@
+package de.unisb.cs.st.javalanche.mutation.runtime.testDriver.junit.data;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestCaseForJunit4Test extends TestCase {
+
+	@Test
+	public void test1() {
+
+	}
+
+	@Test
+	public void test2() {
+
+	}
+
+	@Test
+	public void test3() {
+
+	}
+}
---
+++ src/test/java/de/unisb/cs/st/javalanche/mutation/runtime/testDriver/junit/data/TestCaseForJunitTest.java
@@ -0,0 +1,20 @@
+package de.unisb.cs.st.javalanche.mutation.runtime.testDriver.junit.data;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TestCaseForJunitTest extends TestCase {
+
+	public void test1() {
+
+	}
+
+	public void test2() {
+
+	}
+
+	public void test3() {
+
+	}
+}
+
