e2c267196b01e55776b7b2c37a11c5ac54891faa
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2012-01-30 11:08:59 -0500

    Refactor and fix test-triangle examples
    Add:
     * Documentation for the test scripts
    
    Fix:
     * Scripts so that the tests would pass
    
    Refactor:
     * Format to better separate the tasks
     * Output of results to be more apparent

--- test-triangle-junit4.sh
+++ test-triangle-junit4.sh
@@ -1,25 +1,42 @@
 #! /bin/bash
 VERSION=`grep "<version>" pom.xml | head -1 | sed -E 's/.*0/0/' | sed -E 's/<.*//'`
 DIST=javalanche-$VERSION
+
+# Set up test example
 cd ${DIST}/examples/triangleJunit4
-JAVALANCHE_ARGS="-Dprefix=triangle -Dcp=target/classes -Djavalanche=../../ -Dtests=triangle.tests.Triangle1Test:triangle.tests.Triangle2Test:triangle.tests.Triangle3Test"
 ant test
+cp ../../javalanche.xml .
+
+# Variables representing the two test's properties and their output locations
 OUT1=out-mutationTest-1.txt
 OUT2=out-mutationTest-2.txt
-cp ../../javalanche.xml .
-ant -f javalanche.xml $JAVALANCHE_ARGS mutationTest | tee ${OUT1}
 sed -iBACK 's/<project name="Javalanche">/& \
     <property name="prefix" value="triangle"\/> \
     <property name="tests" value="triangle.tests.TriangleTestSuite"\/> \
     <property name="javalanche" value="..\/..\/"\/> \
-    <property name="cp" value="target\/classes"\/> /' javalanche.xml 
+    <property name="cp" value="target\/classes"\/> /' javalanche.xml
+JAVALANCHE_ARGS="-Dprefix=triangle -Dcp=target/classes -Djavalanche=../../ -Dtests=triangle.tests.Triangle1Test:triangle.tests.Triangle2Test:triangle.tests.Triangle3Test"
+
+# Test 1: using javalanche.xml properties with JUnit TestSuite
+ant -f javalanche.xml $JAVALANCHE_ARGS mutationTest | tee ${OUT1}
+
+# Test 2: using CLI arguments with JUnit Tests (composes testsuite)
 ant -f javalanche.xml mutationTest | tee ${OUT2}
+
+# Clean up test
 ant clean
 rm javalanche.xml
 
-SAME="Same output for both runs"
+# Variables required for the comparison of the output
+SAME="Successful: Same output for both runs"
 GREP1_1="$(grep "Mutation score:" ${OUT1})"
 GREP1_2="$(grep "Mutation score:" ${OUT2})"
+GREP2_1="$(grep "Covered mutations" ${OUT1})"
+GREP2_2="$(grep "Covered mutations" ${OUT2})"
+
+# Output the test results
+echo "-------------------------------------"
+echo "Testing Mutation Score"
 if [ "$GREP1_1" == "$GREP1_2" ]
 then
 	echo  ${SAME}
@@ -27,8 +44,8 @@ else
 	echo  Error: Mutation score not equal. ${GREP1_2} - ${GREP1_1}
 fi
 
-GREP2_1="$(grep "Covered mutations" ${OUT1})"
-GREP2_2="$(grep "Covered mutations" ${OUT2})"
+echo ""
+echo "Testing Covered Mutations"
 if [ "$GREP2_1" == "$GREP2_2" ]
 then
 	echo  ${SAME}
@@ -36,4 +53,6 @@ else
 	echo  Error: Covered mutations not equal. ${GREP2_2} - ${GREP2_1}
 fi
 
-
+# Clean up output files
+rm ${OUT1}
+rm ${OUT2}
